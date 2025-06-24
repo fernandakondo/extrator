c3b36453f71639c32c6d517b465e0c9eb2658209
fernandakondo <fernanda.lutra@escola.pr.gov.br>
2025-06-24 08:34:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const BotaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+BotaoMostraPalavras.addEventListener('click', MostraPalavrasChave);
+
+function MostraPalavrasChave(){
+    alert('fuiclicado!');
+}
